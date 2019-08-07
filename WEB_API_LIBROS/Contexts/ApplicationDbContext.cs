ff0bca62d051175697ac8b450a45de63b94769cc
Richa <Richa@DESKTOP-397FMC3>
2019-08-07 04:52:42 -0400

    Creacion de la carpeta "Contexts" donde se ubicaran mi contesto de datos.

---
+++ WEB_API_LIBROS/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WEB_API_LIBROS.Entities;
+
+namespace WEB_API_LIBROS.Contexts
+{
+    public class ApplicationDbContext: DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Autor> Autores { get; set; }
+    }
+}
