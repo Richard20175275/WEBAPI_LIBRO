9724e47c01d31904e1e1bbb7053b0288ff99a317
Richa <Richa@DESKTOP-397FMC3>
2019-08-07 05:27:42 -0400

    Hemos modificado el controlador de autor para que ya pueda utilizar la base de datos que hemos creado

---
+++ WEB_API_LIBROS/Controllers/AutoresController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WEB_API_LIBROS.Contexts;
+using WEB_API_LIBROS.Entities;
+
+namespace WEB_API_LIBROS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutoresController: ControllerBase
+    {
+
+        private readonly ApplicationDbContext context;
+
+        public AutoresController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Autor>> Get()
+        {
+            return context.Autores.ToList();
+        }
+
+    }
+}
