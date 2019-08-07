8c3215a0e3592da7f3dc63ce8da4d6441a47ad83
Richa <Richa@DESKTOP-397FMC3>
2019-08-07 05:13:56 -0400

    Se ha hecho una migracion

--- WEB_API_LIBROS/Startup.cs
+++ WEB_API_LIBROS/Startup.cs
@@ -27,7 +27,7 @@ namespace WEB_API_LIBROS
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionString")));
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
